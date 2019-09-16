9a5b752fec385658ef1ab04c4d65eb47fd9be082
ryomak <ryooomaaa0413@outlook.com>
2019-09-16 21:59:30 +0900

    init

--- src/front/layouts/default.vue
+++ src/front/layouts/default.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <b-navbar>
-      <template slot="start">
-        <b-navbar-item v-for="(item, key) in items" :key="key" :href="item.to">
+    <header>
+      <div class="menu">
+        <div class="bar-1"></div>
+        <div class="bar-2"></div>
+        <div class="bar-3"></div>
+      </div>
+      <div class="menu-detail" :class="{ open: isOpen }">
+        <div
+          v-for="(item, key) in items"
+          :key="key"
+          :href="item.to"
+          :target="item.blank ? '_blank' : false"
+        >
           <b-icon :icon="item.icon"> </b-icon>
           <span>{{ item.title }}</span>
-        </b-navbar-item>
-        <b-navbar-dropdown label="SNS" animation="slide-next">
-          <b-navbar-item
-            v-for="(s, index) in sns"
-            :key="index"
-            :href="s.to"
-            aria-role="menuitem"
-            target="_blank"
-          >
-            <b-icon :icon="s.icon" :pack="s.pack"></b-icon>
-            <span>{{ s.title }}</span>
-          </b-navbar-item>
-        </b-navbar-dropdown>
-      </template>
-    </b-navbar>
+        </div>
+      </div>
+    </header>
     <div class="container">
       <nuxt />
     </div>
@@ -28,55 +26,81 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      isOpen: false
+    }
   },
   computed: {
-    sns() {
-      return [
-        {
-          title: 'github',
-          icon: 'github-face',
-          pack: 'mdi',
-          to: 'https://github.com/ryomak'
-        },
-        {
-          title: 'twitter',
-          icon: 'twitter',
-          pack: 'mdi',
-          to: 'https://twitter.com/kurisuuu3'
-        },
-        {
-          title: 'instagram',
-          icon: 'instagram',
-          pack: 'mdi',
-          to: 'https://www.instagram.com/kuriiiii13'
-        }
-      ]
-    },
     items() {
       return [
         {
           title: 'Home',
           icon: 'home',
-          to: '/'
+          to: {
+            value: '/',
+            blank: false
+          }
         },
         {
           title: 'Works',
           icon: 'console',
-          to: 'works'
+          to: {
+            value: 'works',
+            blank: false
+          }
         },
         {
           title: 'Blog',
           icon: 'desktop-mac',
-          to: 'https://ryomak.info'
+          to: {
+            value: 'https://ryomak.info',
+            blank: true
+          }
         },
         {
           title: 'Contact',
           icon: 'email',
-          to: 'contact'
+          to: {
+            value: 'contact',
+            blank: false
+          }
+        },
+        {
+          title: 'github',
+          icon: 'github-face',
+          to: {
+            value: 'https://github.com/ryomak',
+            blank: true
+          }
+        },
+        {
+          title: 'twitter',
+          icon: 'twitter',
+          to: {
+            value: 'https://twitter.com/kurisuuu3',
+            blank: true
+          }
+        },
+        {
+          title: 'instagram',
+          icon: 'instagram',
+          to: {
+            value: 'https://www.instagram.com/kuriiiii13',
+            blank: true
+          }
         }
       ]
     }
   }
 }
 </script>
+<style lang="scss">
+.menu-detail {
+  position: fixed;
+  background: rgba(#eee, 0.7);
+  padding: 1rem;
+}
+.open {
+  height: 100vh;
+}
+</style>
